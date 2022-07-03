b5be7582b24983529dd6e7958dc82b9a9c1958cb
Life-pain <lesha.kulakov95@mail.ru>
2022-07-03 23:22:17 +0300

    Initial commit

---
+++ src/ThreadClient.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ThreadClient extends Thread{
+    @Override
+    public void run() {
+        try (Socket clientSocket = new Socket("127.0.0.1", 8090)) {
+            Thread.sleep(3000);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            out.println("client");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
