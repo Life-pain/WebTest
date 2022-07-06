d0db384e67068e61b36b3ee6869969632f844405
Life-pain <lesha.kulakov95@mail.ru>
2022-07-07 01:45:29 +0300

    refactoring

--- src/ThreadServer.java
+++ src/ThreadServer.java
@@ -5,18 +5,20 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ThreadServer extends Thread{
+public class ThreadServer extends Thread {
     @Override
     public void run() {
-        try (ServerSocket serverSocket = new ServerSocket(8090)){
+        try (ServerSocket serverSocket = new ServerSocket(8090)) {
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                System.out.println("New connection accepted");
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-                final String name = in.readLine();
-                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                    System.out.println("New connection accepted");
+
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
